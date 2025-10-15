3330623a3096a5148198748d6f1b10a40ca3f5f5
Nikhita Chinmai <147228315+NikhitaChinmai@users.noreply.github.com>
2025-10-15 12:18:38 +0530

    Create advance.js

---
+++ advance.js
@@ -0,0 +1,7 @@
+document.getElementById('savings-button').addEventListener('click', function() {
+    alert('Savings Sentinel clicked!');
+});
+
+document.getElementById('loan-button').addEventListener('click', function() {
+    alert('Loan Sphere clicked!');
+});
